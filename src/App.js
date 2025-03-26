9c0a9c89f2b13a15873b50eb6b49ddfb8b053477
LEONARDO AMANO <leonardo.amano@delfia.tech>
2025-03-26 14:11:33 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
+import Head from '../src/Componentes/Head/Head'
+import Header from '../src/Componentes/Header/Header';
+import Footer from '../src/Componentes/Footer/Footer';
+import Main from '../src/Componentes/Main/Main';
+import Sidebar from '../src/Componentes/Sidebar/Sidebar';
+import Script from '../src/Componentes/Script';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+    <Head/>
+    <Header/>
+    <Main/>
+    <Sidebar/>
+    <Footer/>
+    <Script/>
     </div>
   );
 }
---
+++ src/Componentes/Head/Head.js
@@ -0,0 +1,12 @@
+import icon from '../../assets/icons/favicon.png'
+
+const Head = () => {
+    return (
+        <head>
+            <title>Imersão Alura</title>
+            <link rel="icon" href={icon} type='image/png'></link>
+        </head>
+    );
+}
+
+export default Head;
